ea6ff8cfdc5caf6e7e4dd5c65d8391d82071f099
ProklUng <funtik21975@gmail.com>
2021-07-15 08:07:59 +0300

    Рефакторинг

--- .settings.php
+++ .settings.php
@@ -4,7 +4,7 @@ return [
     'parameters' => [
         'value' => [
             'cache_path' => '/bitrix/cache/s1/proklung.redis', // Путь к закешированному контейнеру
-            'compile_container_envs' => ['dev', 'prod'], // Окружения при которых компилировать контейнер
+            'compile_container_envs' => ['prod'], // Окружения при которых компилировать контейнер
             'container.dumper.inline_factories' => false, // Дампить контейнер как одиночные файлы
         ],
         'readonly' => false,
